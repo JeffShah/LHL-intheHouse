f9266281550a5d455db7dfea81e5c0ce409db080
JaffarShah <sjeffshah@gmail.com>
2019-04-03 14:04:26 -0400

    Modified and deleted repeated code

--- countLetters.js
+++ countLetters.js
@@ -20,22 +20,3 @@ console.log(countLetters("Lighthouse in the house"));
 // defining var and using if/else statement
 // i as previously defined = 0 and is < lhlHouse.length
 // split with space, while join without spaces
-
-function countLetters(word) {
-
-  var lhlHouse = word.split(" ").join("");
-  var outputLetters = {};
-  for (var i = 0; i < lhlHouse.length; i++) {
- //console.log(i); //i represent each of the positions
- //console.log(lhlHouse[i]);
- var letter = lhlHouse[i];
-    if (letter in outputLetters) {
-    outputLetters[letter] += 1;
-
-    } else {
-    outputLetters[letter]= 1;
-    }
-  }
-  return outputLetters;
-}
-console.log(countLetters("Lighthouse in the house"));
